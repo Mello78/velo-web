d34362de0a19840a1812b7f9130dc490231ea0b4
Mello <mello.cn@gmail.com>
2026-03-23 21:51:44 +0100

    chore: rimuovi script utility temporanei

--- check.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('C:/Users/mello/velo-web-temp/app/vendor/page.tsx', 'utf8');
-
-// Fix 1: translateToEnglish - aggiungi setBioEn e altri setter dopo data.error check
-const oldAfterError = c.substring(24452, 24452 + 300);
-console.log('Section to replace:', JSON.stringify(oldAfterError));
--- fix_encoding.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('C:/Users/mello/velo-web-temp/app/vendor/page.tsx', 'latin1');
-const buf = Buffer.from(content, 'latin1');
-content = buf.toString('utf8');
-fs.writeFileSync('C:/Users/mello/velo-web-temp/app/vendor/page.tsx', content, 'utf8');
-console.log('Done, length:', content.length, 'has emoji:', content.includes('\u{1F4F7}'));
